98184166041c2dce96228900745b78ae92db71a9
MORALES NAVAS DENNIS ALEXANDER <136649929+alexander20020416@users.noreply.github.com>
2025-10-05 20:23:52 -0500

    Delete src directory

--- src/DataAccessComponent/DataRepository.java
+++
@@ -1,4 +0,0 @@
-// Clase placeholder para la capa de acceso a datos
-public class DataRepository {
-    // TODO: implementar métodos de acceso a datos
-}
--- src/UserInterface/Controller.java
+++
@@ -1,4 +0,0 @@
-// Clase placeholder para la capa de interfaz de usuario
-public class Controller {
-    // TODO: implementar controladores y lógica de presentación
-}
